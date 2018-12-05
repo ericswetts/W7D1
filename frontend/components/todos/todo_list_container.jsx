f1c6daebdae8139744a853f557cbcf04fda6f3b1
ericswetts <eric.swetts@gmail.com>
2018-12-04 18:19:15 -0800

    Conclude W7D2 project

--- frontend/components/todos/todo_list_container.jsx
+++ frontend/components/todos/todo_list_container.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import TodoList from './todo_list';
-import allTodos from "../../reducers/selectors"
+import allTodos from "../../reducers/selectors";
 
 const mapStateToProps = state => ({
   todos: allTodos(state)
